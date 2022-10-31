b1e04f6ba8e5ce1867474a028f441122d67ad315
nemonemoti <nemoto.tech127@gmail.com>
2022-10-31 10:36:47 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do  root to: 'items#index'
-  # devise_for :users
+Rails.application.routes.draw do
+  root to: 'items#index'
+  devise_for :users
   resources :items do
     resources :orders, only: [:index, :create]
   end
---
+++ db/migrate/20221031004530_add_column_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :family_name, :string, null: false
+    add_column :users, :first_name_kana, :string, null: false
+    add_column :users, :family_name_kana, :string, null: false
+    add_column :users, :birth_day, :date, null: false
+  end
+end
