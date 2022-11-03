c4793534fddf4f9e1ea22ac57428141a3eaf02aa
nemonemoti <nemoto.tech127@gmail.com>
2022-11-03 18:09:01 +0900

    add items edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -23,21 +23,21 @@ class ItemsController < ApplicationController
   def show
   end
 
-  # def edit
-  #   if @item.user == current_user
-  #     render 'edit'
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def edit
+    if @item.user == current_user
+      render 'edit'
+    else
+      redirect_to root_path
+    end
+  end
 
-  # def update
-  #   if @item.update(item_params)
-  #     redirect_to(@item)
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    if @item.update(item_params)
+      redirect_to(@item)
+    else
+      render :edit
+    end
+  end
 
   # def destroy
   #   @item.destroy if current_user.id == @item.user_id
