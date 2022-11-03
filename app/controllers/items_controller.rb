37b6655c3d74e3c9833bd3a9b166a7e65b4cc6ba
nemonemoti <nemoto.tech127@gmail.com>
2022-11-03 18:57:07 +0900

    add items del

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -39,12 +39,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #   @item.destroy if current_user.id == @item.user_id
-  #   redirect_to root_path
-  # end
+  def destroy
+    @item.destroy if current_user.id == @item.user_id
+    redirect_to root_path
+  end
 
-  # private
+  private
 
   def set_item
     @item = Item.find(params[:id])
